220814e8a7422c938144df33344721b71c97acd4
gkotecki <gabrielkotecki@gmail.com>
2023-03-16 22:20:31 -0300

    ADD: leaflet deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Tangram from 'tangram';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <main className="flex h-screen w-screen flex-col items-center justify-center bg-neutral-900">
-      <span className="text-2xl text-neutral-200">Vite + React + TS + TailwindCSS</span>
-    </main>
-  </React.StrictMode>
+    <NewFunction />
+  </React.StrictMode>,
 );
+
+function NewFunction() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
